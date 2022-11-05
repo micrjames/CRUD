f9171cf2b94a36ac5437a0a3bd625aa8788bd4a2
Michael James <micrjames921@gmail.com>
2022-11-05 02:02:41 -0700

    setup basic code for project

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+const conn = require("./conn");
+
+/*
+mongoose.connect(`${conn.DB.HOST}:${conn.DB.PORT}/users`);
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to DB ..."));
+*/
+
+const PORT = conn.PORT;
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT} ...`);
+});
